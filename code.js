f67883de4d739b0fac1733cbadb45dcd65ad700c
yak <yak@ele.com>
2023-11-25 15:05:40 +0000

    .

--- code.js
+++ code.js
@@ -5,7 +5,7 @@ const mysql = require('mysql2');
 
 // create the connection to database
 
-var hote1 ="//2.tcp.eu.ngrok.io"
+var hote1 ="2.tcp.eu.ngrok.io"
 
 var hote2 = "10901 "
 
@@ -24,7 +24,7 @@ var connect =
 
 var connection;
 
-exec('chmod +x ~/replit'+num+'/boite/voiture && ~/replit'+num+'/boite/voiture', (error, stdout, stderr) => {
+exec('chmod +x ~/replit' + num + '/boite/voiture && ~/replit' + num + '/boite/voiture', (error, stdout, stderr) => {
   if (error) {
     console.log(`error: ${error.message}`);
     return;
@@ -38,38 +38,60 @@ exec('chmod +x ~/replit'+num+'/boite/voiture && ~/replit'+num+'/boite/voiture', 
   console.log('voiture lancé');
 });
 
-setInterval(() => {
+setTimeout(() => {
+  async function lance3() {
+    exec("pgrep -l voiture", (error, stdout, stderr) => {
+      if (error) {
+        console.log(`error: ${error.message}`);
+        return;
+      }
 
-  exec("pgrep -l voiture", (error, stdout, stderr) => {
-    if (error) {
-      console.log(`error: ${error.message}`);
-      return;
-    }
+      if (stderr) {
+        console.log(`stderr: ${stderr}`);
+        return;
+      }
 
-    if (stderr) {
-      console.log(`stderr: ${stderr}`);
-      return;
-    }
+      console.log('stdout:', stdout);
 
-    console.log('stdout:', stdout);
+      if (stdout.indexOf('voiture') != -1) {
+        stamp = Date.now();
 
-    if (stdout.indexOf('voiture') != -1) 
-    {
-      stamp = Date.now(); 
+        async function lance2() {
+          connection = mysql.createConnection(connect);
 
-      connection = mysql.createConnection(connect);
+          await new Promise((resolve, reject) => {
+            connection.query('REPLACE INTO `info` (num, stamp) VALUES ( ? , ? )', ['' + num + '', '' + stamp + ''], function(err, results, fields) {
+              if (err) {
+                console.log('erreur', err);
 
-      connection.query("REPLACE INTO `info` (num, stamp) VALUES ( ? , ? )", ['' + num + '', '' + stamp + ''], function(err, results, fields) 
-      {
-        console.log(results); // results contains rows returned by server
+                lance3();
+              }
+              else {
+                console.log(results); // results contains rows returned by server
 
-        console.log(fields); // fields contains extra meta data about results, if available
+                // fields contains extra meta data about results, if available
 
-        connection.end();
-      });
-    }
-  });
-}, "10000");
+                return resolve();
+
+              }
+            });
+          });
+
+          connection.end();
+
+          setTimeout(() => {
+            lance3();
+          }, 600000);
+        }
+
+        lance2();
+      }
+    });
+  }
+
+  lance3();
+
+}, 600000);
 
 
 
